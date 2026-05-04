e3ad252e856be71766f11e30ed83d33e8ca0030d
shainapathania948-ux <shainapathania948@gmail.com>
2026-05-04 20:53:04 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -277,31 +277,28 @@ if st.session_state.logged_in:
                     "Random Forest": RandomForestRegressor()
                 }
                 results=[]
-             for name,model in models.items():
-                    model.fit(X_train,y_train)
-                    preds=model.predict(X_test)
-                    r2 = r2_score(y_test, preds)
-                    mae = mean_absolute_error(y_test, preds)
-                    results.append({
-                    "Model": name,
-                    "R2 Score": round(r2, 3),
-                    "MAE": round(mae, 3)
-                    })
-            result_df = pd.DataFrame(results)
-
-            st.write("### 📊 Model Results")
-            st.dataframe(result_df)
-                    #-------------charts
-            st.write("### 📈 Performance Comparison")
-
-            chart = px.bar(result_df, x="Model", y="R2 Score", title="R2 Score Comparison")
-            st.plotly_chart(chart)
-
-            chart2 = px.bar(result_df, x="Model", y="MAE", title="MAE Comparison")
-            st.plotly_chart(chart2)
+                              for name,model in models.items():
+                                  model.fit(X_train,y_train)
+                                  preds=model.predict(X_test)
+                                  r2 = r2_score(y_test, preds)
+                                  mae = mean_absolute_error(y_test, preds)
+                                  results.append({
+                                       "Model": name,
+                                       "R2 Score": round(r2, 3),
+                                       "MAE": round(mae, 3)
+                                    })
+                                 result_df = pd.DataFrame(results)
+                                 st.write("### 📊 Model Results")
+                                 st.dataframe(result_df)
+                    #-------------charts----------
+                                 st.write("### 📈 Performance Comparison")
+                                 chart = px.bar(result_df, x="Model", y="R2 Score", title="R2 Score Comparison")
+                                 st.plotly_chart(chart)
+                                 chart2 = px.bar(result_df, x="Model", y="MAE", title="MAE Comparison")
+                                 st.plotly_chart(chart2)
                     #-----------------Best Mode----------
-            best_model = result_df.sort_values(by="R2 Score", ascending=False).iloc[0]
-            st.success(f"🏆 Best Model: {best_model['Model']}")
+                                best_model = result_df.sort_values(by="R2 Score", ascending=False).iloc[0]
+                                st.success(f"🏆 Best Model: {best_model['Model']}")
 
 
                 
